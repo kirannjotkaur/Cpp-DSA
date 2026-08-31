6571d3fa6e3125c8d49bb5781e70c89ef4e87b75
Kirannn <kaurkirannjot@gmail.com>
2026-08-31 17:26:20 +0530

    day one of dsa cpp

--- DAYONE/AlphaSq.cpp
+++ DAYONE/AlphaSq.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 int main()
 {
-    int n = 4;
+    int n = 3;
+    char ch = 'A';
     for (int i = 0; i < n; i++)
     {
         for (int i = 0; i < n; i++)
         {
-            cout << "*";
+            cout << ch << " ";
+            ch = ch+1;
         }
         cout << endl;
     }
